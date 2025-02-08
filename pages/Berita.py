38c5c42692785d91937d9944ec28649a84009fe7
Abdul Rosyid <abdulrosyid6122004@gmail.com>
2025-02-08 15:33:30 +0700

    Update Berita.py

--- pages/Berita.py
+++ pages/Berita.py
@@ -83,13 +83,19 @@ with col_header2:
         📧 Email: [info@digicek.com](mailto:info@digicek.com)
         """)
 
-# DATA BERITA
+# DATA BERITA dengan URL
 berita_data = [
-    {"title": "IDAI Sebut Anak dengan Diabetes Lebih Rentan Terkena Gagal Ginjal, Ini Alasannya", "description": "Rabu, 27 November 2024","image":"image/gagal_ginjal.jpeg"},
-    {"title": "Tren Penyakit Diabetes di Atas Usia 15 Tahun Meningkat, Pecinta Makanan Manis Penting Lakukan Ini", "description": "Kamis, 30 Januari 2025","image":"image/diabetes.webp"},
+    {"title": "IDAI Sebut Anak dengan Diabetes Lebih Rentan Terkena Gagal Ginjal, Ini Alasannya", 
+     "description": "Rabu, 27 November 2024",
+     "image": "image/gagal_ginjal.jpeg",
+     "url": "https://health.detik.com/berita-detikhealth/d-7657862/idai-sebut-anak-dengan-diabetes-lebih-rentan-terkena-gagal-ginjal-ini-alasannya#:~:text=Ikatan%20Dokter%20Anak%20Indonesia%20(IDAI,dalam%20batas%20normal%20terbilang%20buruk."},
+
+    {"title": "Tren Penyakit Diabetes di Atas Usia 15 Tahun Meningkat, Pecinta Makanan Manis Penting Lakukan Ini", 
+     "description": "Kamis, 30 Januari 2025",
+     "image": "image/diabetes.webp",
+     "url": "https://mediaindonesia.com/humaniora/739296/tren-penyakit-diabetes-di-atas-usia-15-tahun-meningkat-pecinta-makanan--manis-penting-lakukan-ini"},
 ]
 
-# CAROUSEL BERITA
 st.title("Berita")
 
 berita_cols = st.columns(len(berita_data))
@@ -99,3 +105,4 @@ for idx, berita in enumerate(berita_data):
         st.image(berita["image"], width=300)
         st.markdown(f"**{berita['title']}**")
         st.write(berita["description"])
+        st.link_button("Baca Selengkapnya", berita["url"])
